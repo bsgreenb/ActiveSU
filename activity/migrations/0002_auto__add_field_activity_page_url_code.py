d174eddbeda3f2d83788392a9b379b318740ade6
Ben Greenberg <bsgreenb@gmail.com>
2012-02-15 13:02:56 -0800

    Ok, lets see how this works

---
+++ activity/migrations/0002_auto__add_field_activity_page_url_code.py
@@ -0,0 +1,106 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Activity_Page.url_code'
+        db.add_column('activity_activity_page', 'url_code', self.gf('django.db.models.fields.CharField')(default='', max_length=50), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Activity_Page.url_code'
+        db.delete_column('activity_activity_page', 'url_code')
+
+
+    models = {
+        'activity.activity_page': {
+            'Meta': {'object_name': 'Activity_Page'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'url_code': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.User']", 'through': "orm['activity.Activity_Page_Users']", 'symmetrical': 'False'})
+        },
+        'activity.activity_page_users': {
+            'Meta': {'object_name': 'Activity_Page_Users'},
+            'activity_page': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['activity.Activity_Page']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'join_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'activity.comment': {
+            'Meta': {'object_name': 'Comment'},
+            'comment_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'content': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'post': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['activity.Post']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'activity.event_post': {
+            'Meta': {'object_name': 'Event_Post'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'end_datetime': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'post': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['activity.Post']", 'unique': 'True'}),
+            'start_datetime': ('django.db.models.fields.DateTimeField', [], {}),
+            'where': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'activity.post': {
+            'Meta': {'object_name': 'Post'},
+            'activity_page': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['activity.Activity_Page']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'post_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'activity.text_post': {
+            'Meta': {'object_name': 'Text_Post'},
+            'content': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'post': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['activity.Post']", 'unique': 'True'})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['activity']
---
+++ activity/migrations/0003_initial.py
@@ -0,0 +1,175 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Activity_Page'
+        db.create_table('activity_activity_page', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('url_code', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('enabled', self.gf('django.db.models.fields.BooleanField')(default=True)),
+        ))
+        db.send_create_signal('activity', ['Activity_Page'])
+
+        # Adding model 'Activity_Page_Users'
+        db.create_table('activity_activity_page_users', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('activity_page', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['activity.Activity_Page'])),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('join_time', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal('activity', ['Activity_Page_Users'])
+
+        # Adding model 'Post'
+        db.create_table('activity_post', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('activity_page', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['activity.Activity_Page'])),
+            ('post_time', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal('activity', ['Post'])
+
+        # Adding model 'Text_Post'
+        db.create_table('activity_text_post', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('post', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['activity.Post'], unique=True)),
+            ('content', self.gf('django.db.models.fields.CharField')(max_length=500)),
+        ))
+        db.send_create_signal('activity', ['Text_Post'])
+
+        # Adding model 'Event_Post'
+        db.create_table('activity_event_post', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('post', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['activity.Post'], unique=True)),
+            ('where', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('start_datetime', self.gf('django.db.models.fields.DateTimeField')()),
+            ('end_datetime', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('description', self.gf('django.db.models.fields.CharField')(max_length=500)),
+        ))
+        db.send_create_signal('activity', ['Event_Post'])
+
+        # Adding model 'Comment'
+        db.create_table('activity_comment', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('post', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['activity.Post'])),
+            ('content', self.gf('django.db.models.fields.CharField')(max_length=250)),
+            ('comment_time', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal('activity', ['Comment'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Activity_Page'
+        db.delete_table('activity_activity_page')
+
+        # Deleting model 'Activity_Page_Users'
+        db.delete_table('activity_activity_page_users')
+
+        # Deleting model 'Post'
+        db.delete_table('activity_post')
+
+        # Deleting model 'Text_Post'
+        db.delete_table('activity_text_post')
+
+        # Deleting model 'Event_Post'
+        db.delete_table('activity_event_post')
+
+        # Deleting model 'Comment'
+        db.delete_table('activity_comment')
+
+
+    models = {
+        'activity.activity_page': {
+            'Meta': {'object_name': 'Activity_Page'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'url_code': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.User']", 'through': "orm['activity.Activity_Page_Users']", 'symmetrical': 'False'})
+        },
+        'activity.activity_page_users': {
+            'Meta': {'object_name': 'Activity_Page_Users'},
+            'activity_page': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['activity.Activity_Page']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'join_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'activity.comment': {
+            'Meta': {'object_name': 'Comment'},
+            'comment_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'content': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'post': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['activity.Post']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'activity.event_post': {
+            'Meta': {'object_name': 'Event_Post'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'end_datetime': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'post': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['activity.Post']", 'unique': 'True'}),
+            'start_datetime': ('django.db.models.fields.DateTimeField', [], {}),
+            'where': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'activity.post': {
+            'Meta': {'object_name': 'Post'},
+            'activity_page': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['activity.Activity_Page']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'post_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'activity.text_post': {
+            'Meta': {'object_name': 'Text_Post'},
+            'content': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'post': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['activity.Post']", 'unique': 'True'})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['activity']
