2fe85f0a4c2b415f4c4a70325d4fc879cf666881
Ben Greenberg <bsgreenb@gmail.com>
2012-02-16 15:26:44 -0800

    Got the raw query written up

--- activity/models.py
+++ activity/models.py
@@ -2,10 +2,41 @@ from django.db import models
 from django.db.models import Max, Count
 from django.contrib.auth.models import User
 
+from utils import ReverseManager
+
 #from django.db import connection #for debugging
 
 #Hmm lets see.  We want to get the Activity_Page LEFT JOIN (Post 
 #
+
+def get_activities_with_top_posts():
+    """Returns all activities with their most recent posts.  We use raw SQL because django 1.3 doesn't have prefetch_related().  This is the only way to do it in one query."""
+    return Activity_Page.objects.raw('''
+    SELECT activity_activity_page.name, activity_activity_page.url_code,
+    posts.*
+    auth_user.username,
+    activity_text_post.content,
+    activity_event_post.title
+    FROM
+    activity_activity_page
+    LEFT JOIN
+    (
+        SELECT activity_post.activity_page_id, activity_post.event_time,
+        auth_user.username,
+        activity_text_post.content,
+        activity_event_post.title
+        FROM
+        activity_post
+         ON activity_post.activity_page_id = activity_activity_page.id
+        LEFT JOIN
+        activity_text_post
+         ON activity_post.id = activity_text_post.post_id
+        LEFT JOIN
+        activity_event_post
+         ON activity_post.id = activity_event_post.post_id
+    ) as posts
+     ON posts.activity_page_id = activity_activity_page.id
+    ''')
 
 class Activity_Page(models.Model):
     name = models.CharField(max_length=50)
@@ -32,6 +63,10 @@ class Activity_Page(models.Model):
         #TODO: This is proly wrong
         return Event_Post.objects.filter(post__activity_page=self).order_by('start_datetime', '-comment__comment_time')
 
+    #Reverse manager so we can prefetch all the posts
+    objects = models.Manager()
+    reversemanager = ReverseManager({'posts': 'post_set'})
+
 class Activity_Page_User(models.Model):
     activity_page = models.ForeignKey(Activity_Page)
     user = models.ForeignKey(User)
@@ -42,9 +77,12 @@ class Post(models.Model):
     user = models.ForeignKey(User)
     activity_page = models.ForeignKey(Activity_Page)
     post_time = models.DateTimeField(auto_now_add = True)
-
+    
     def __unicode__(self):
         return 'Post by ' + unicode(self.user) + ' on ' + unicode(self.activity_page) + ' at ' + str(self.post_time)
+    
+    class Meta:
+        get_latest_by = 'post_time'
 
 class Text_Post(models.Model):
     post = models.OneToOneField(Post)
