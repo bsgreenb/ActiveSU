1bd67bbee33b3c7377a7ca7c792847e224be6680
Ben Greenberg <bsgreenb@gmail.com>
2012-02-17 14:45:22 -0800

    get ordering right

--- activity/models.py
+++ activity/models.py
@@ -76,7 +76,7 @@ class Comment(models.Model):
     comment_time = models.DateTimeField(auto_now_add = True)
 
     class Meta:
-        ordering = ['-comment_time']
+        ordering = ['comment_time']
     
     def __unicode__(self):
         return 'Comment by ' + str(self.user) + ' on ' + str(self.post) + ':' + self.content
