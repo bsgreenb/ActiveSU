d9d77f8029c57f928cf52548e6a38709a382d30c
rui xia <ruixia21@gmail.com>
2012-02-19 10:38:44 -0800

    changed the text of emails

--- activity/library/send_mail.py
+++ activity/library/send_mail.py
@@ -7,6 +7,10 @@ from django.template import Context
 # get from SO: http://stackoverflow.com/questions/2809547/creating-email-templates-with-django
 
 def send_registration_confirmation(user):
+    SUBJECT = 'Welcome to ActiveSU'
+    FROM_EMAIL = 'no-reply@activesu.com'
+    TO = user.email
+
     p = user.get_profile()
     plaintext = get_template('emails/registration/confirmation_email.txt')
     htmly = get_template('emails/registration/confirmation_email.html')
@@ -16,16 +20,20 @@ def send_registration_confirmation(user):
         'username':user.username
     })
 
-    subject, from_email, to = ('', '', '')
     text_content = plaintext.render(data)
     html_content = htmly.render(data)
 
-    message = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    message = EmailMultiAlternatives(SUBJECT, text_content, FROM_EMAIL, [TO])
     message.attach_alternative(html_content, "text/html")
     message.send()
 
 
 def send_email_to_post(post, content, to_user):
+
+    SUBJECT = 'You got a message from ActiveSU'
+    FROM_EMAIL = post.user.email
+    TO = to_user.email
+
     plaintext = get_template('emails/send_email_to_post/message.txt')
     htmly = get_template('emails/send_email_to_post/message.html')
 
@@ -35,10 +43,9 @@ def send_email_to_post(post, content, to_user):
         'content':content
     })
 
-    subject, from_email, to = ('', '', '')
     text_content = plaintext.render(data)
     html_content = htmly.render(data)
 
-    message = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    message = EmailMultiAlternatives(SUBJECT, text_content, FROM_EMAIL, [TO])
     message.attach_alternative(html_content, "text/html")
     message.send()
