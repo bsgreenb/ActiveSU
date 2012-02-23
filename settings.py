1a082c253b8012b0f988ab7fac4f42577e4a422e
Ben Greenberg <ben@textyard.com>
2012-02-23 20:36:19 +0000

    fixing up settings things

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from settings_local import *
 import platform
 
 comp_name = platform.node()
-if comp_name == 'cloudserver': #our production cloud site server's name
+if comp_name == 'textyard.com': #our production cloud site server's name
     from settings_production import *
 else:
     from settings_development import *
