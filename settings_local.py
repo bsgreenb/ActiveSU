0350e0ec2dd1838f0e905d2c0b9c2525c4dd87e4
rui xia <ruixia21@gmail.com>
2012-02-23 12:26:51 -0800

    get site name

--- settings_local.py
+++ settings_local.py
@@ -14,7 +14,8 @@ TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
                                "django.core.context_processors.media",
                                "django.core.context_processors.static",
                                "django.contrib.messages.context_processors.messages",
-                               "django.core.context_processors.request",)
+                               "django.core.context_processors.request",
+                               "activity.context_processor.get_site_name")
 
 
 MANAGERS = ADMINS
