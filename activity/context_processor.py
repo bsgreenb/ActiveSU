9922f4c3fa335efb8ce12b2c8c60b46a2788ac0c
Ben Greenberg <bsgreenb@gmail.com>
2012-02-24 00:55:02 +0000

    trying to merge here..

--- activity/context_processor.py
+++ activity/context_processor.py
@@ -13,4 +13,7 @@ def get_site_name(request):
 
 if __name__ == '__main__':
     print settings.SITE_NAME
+<<<<<<< HEAD
+>>>>>>> a02d13173ff0014c5b7f1c2e134ab3d871d0dc5b
+=======
 >>>>>>> a02d13173ff0014c5b7f1c2e134ab3d871d0dc5b
